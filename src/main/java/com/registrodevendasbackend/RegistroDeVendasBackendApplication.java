592b2c0d539a346f30580bbb24928ba2c00247d0
Marcelo Wendel <mwbc.cs@gmail.com>
2024-04-27 15:09:28 -0300

    commit inicial

---
+++ src/main/java/com/registrodevendasbackend/RegistroDeVendasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.registrodevendasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroDeVendasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroDeVendasBackendApplication.class, args);
+	}
+
+}
