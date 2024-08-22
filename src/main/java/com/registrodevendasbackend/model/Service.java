78e2cf2efe2349a6982ee60895c081f6fbf12499
Marcelo Wendel <mwbc.cs@gmail.com>
2024-08-21 23:08:51 -0300

    Adicionando dependencia para spring validation. Utilizando lombok nos models de User e Service. Adicionando path para o servlet.

--- src/main/java/com/registrodevendasbackend/model/Service.java
+++ src/main/java/com/registrodevendasbackend/model/Service.java
@@ -9,22 +9,30 @@ import org.hibernate.annotations.SQLRestriction;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "services")
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @SQLDelete(sql = "UPDATE services SET deleted = true WHERE id = ?")
 @SQLRestriction("deleted = false")
 public class Service {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID id;
 	
 	@Column(name = "title")
@@ -48,4 +56,7 @@ public class Service {
 	
 	@Column(name = "updated_at")
 	private Timestamp updatedAt;
+	
+	@Transient
+	private UUID userId;
 }
--- src/main/java/com/registrodevendasbackend/model/User.java
+++ src/main/java/com/registrodevendasbackend/model/User.java
@@ -5,18 +5,25 @@ import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID id;
 	
 	@Column(name = "name")
